f7162fa3475d28596ff7a6bd8f788829fdf80f1d
diegomartinez2 <diegomartinez2@yahoo.es>
2024-05-29 15:44:45 +0200

    Just copying from example...

--- Player.gd
+++ Player.gd
@@ -1,23 +1,24 @@
 extends Area2D
 
+signal hit
+
 @export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed(&"move_right"):
 		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed(&"move_left"):
 		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
+	if Input.is_action_pressed(&"move_down"):
 		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
+	if Input.is_action_pressed(&"move_up"):
 		velocity.y -= 1
 
 	if velocity.length() > 0:
@@ -28,3 +29,29 @@ func _process(delta):
 
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
+
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = &"right"
+		$AnimatedSprite2D.flip_v = false
+		$Trail.rotation = 0
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = &"up"
+		if velocity.y > 0:
+			rotation = PI
+		else:
+			rotation = 0
+
+
+func start(pos):
+	position = pos
+	rotation = 0
+	show()
+	$CollisionShape2D.disabled = false
+
+
+func _on_Player_body_entered(_body):
+	hide() # Player disappears after being hit.
+	hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred(&"disabled", true)
