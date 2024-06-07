9dc5ab2d44a17725a612fbc04cdc8eeef19a35a5
diegomartinez2 <diegomartinez2@yahoo.es>
2024-06-07 13:16:34 +0200

    update

---
+++ character_body_2d.gd
@@ -0,0 +1,12 @@
+extends Area2D
+var screen_size # Size of the game window.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
