0fe6d770bcafcc59a35cb44a28f0b54dee2531ad
diegomartinez2 <diegomartinez2@yahoo.es>
2024-06-07 13:28:50 +0200

    update

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,5 +1,8 @@
 extends Area2D
 var screen_size # Size of the game window.
+@export var speed = 400
+var target = position
+var velocity = Vector2.ZERO # The player's movement vector.
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,3 +13,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _input(event):
+	if event.is_action_pressed("click"):
+		target = get_global_mouse_position()
+
+func _physics_process(delta):
+	velocity = position.direction_to(target) * speed
+	# look_at(target)
+	if position.distance_to(target) > 10:
+		move_and_slide()
